17455749e73ef64f13a4efe8265e35f885a30f5f
Sp3rick <davidemar2005@gmail.com>
2026-01-22 14:42:37 +0100

    fix base redirect

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import {Redirect} from '@docusaurus/router';
 
 export default function Home() {
-  return <Redirect to="/orivon-docs/docs/orivon" />;
+  return <Redirect to="/docs/orivon" />;
 }
